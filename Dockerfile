fbd3e46e746bed9eed88f17a7392da2872f34203
thodt6@gmail.com <thodt6@gmail.com>
2022-10-31 17:05:56 +0700

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8u141-jre
 VOLUME /opt/app
-ADD target/ms_invester.jar /opt/app/ms_vngeo.jar
+ADD target/ms_vngeo.jar /opt/app/ms_vngeo.jar
 WORKDIR /opt/app
 ENTRYPOINT exec java -jar /opt/app/ms_vngeo.jar
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,18 +4,18 @@ CREATE TABLE IF NOT EXISTS `province` (
     `type` varchar(30) CHARACTER SET utf8 NOT NULL,
     `slug` varchar(30) DEFAULT NULL,
     PRIMARY KEY (`matp`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `district` (
     `maqh` varchar(5) CHARACTER SET utf8 NOT NULL,
     `name` varchar(100) CHARACTER SET utf8 NOT NULL,
     `type` varchar(30) CHARACTER SET utf8 NOT NULL,
     `matp` varchar(5) CHARACTER SET utf8 NOT NULL,
     PRIMARY KEY (`maqh`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `ward` (
     `xaid` varchar(5) CHARACTER SET utf8 NOT NULL,
     `name` varchar(100) CHARACTER SET utf8 NOT NULL,
     `type` varchar(30) CHARACTER SET utf8 NOT NULL,
     `maqh` varchar(5) CHARACTER SET utf8 NOT NULL,
     PRIMARY KEY (`xaid`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
